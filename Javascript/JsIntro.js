02899e1992eb1a99579dac6a58a09027cbc3e57e
lakshay122007 <lakshay11goyal@gmail.com>
2025-11-20 09:55:10 +0530

    JS Intro

---
+++ Javascript/JsIntro.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!")
+console.warn("This is a warning message.")
+console.error("This is an error message.")
+console.info("This is some informational message.")
+
+console.debug("This is a debug message.")
+console.log("This is a log message.")
+console.table({name: "Alice", age: 30, city: "New York"});
+console.group("User Info");
+
+console.log("Name: Alice");
+console.log("Age: 30");
+console.log("City: New York");
+console.groupEnd();
+console.count("Counter");
+console.count("Counter");
+console.count("Counter");
+console.countReset("Counter");
+console.trace("Trace message");
+console.timeStamp("Time Stamp Message");
+
+
+
+
+
+
+
