b9aa01cc75c309c6800107bf1b1e3be849bf2d72
lakshay122007 <lakshay11goyal@gmail.com>
2025-11-27 13:34:01 +0530

    Js part 2

---
+++ Javascript/Js_2.js
@@ -0,0 +1,100 @@
+//                      // <--------------------------------------------------------------------------------------------------------------->
+
+// console.log("Hello, World!");   
+
+//                      // <--------------------------------------------------------------------------------------------------------------->
+
+
+// const student = {
+//     name: "Lakshay",
+//     age: 18,
+//     course: "B.Tech"
+// }
+// console.log(`Student Name: ${student.name}, Age: ${student.age}, Course: ${student.course}`);
+// student.name = "Goyal";
+// console.log(`Updated Student Name: ${student.name}`);
+// student.age += 1;
+// console.log(`Updated Student Age: ${student.age}`);
+// student.course = "M.Tech";
+// console.log(`Updated Student Course: ${student.course}`);   
+
+// console.table(student);
+
+
+//                     // <--------------------------------------------------------------------------------------------------------------->
+
+
+// function greet(name) {
+//     return `Hello, ${name}! Welcome to JavaScript programming.`;
+// }
+// console.log(greet("Lakshay"));
+
+
+
+//                     // <--------------------------------------------------------------------------------------------------------------->
+
+
+// let a = 10;
+// let b = 20;
+// let sum = a + b;
+// console.log("sum of a and b is:", ++sum)
+
+// console.table(student);
+
+
+//                     // <--------------------------------------------------------------------------------------------------------------->
+
+
+
+// function maggie(ingridients) {
+//     console.log("bring maggie");
+//     console.log("boil water");
+//     console.log("add masala");
+//     console.log("add maggie");
+//     console.log("wait for 2 minutes");
+//     console.log("serve hot maggie");
+//     console.log("JAi maheshmati");
+//     let a = "Maggie is ready to be served";
+//     return a;
+// }
+// console.log(maggie());
+
+
+//                     // <--------------------------------------------------------------------------------------------------------------->
+
+
+// let arr = [1,2,3,4,5,"Lakshay","Goyal",true,false];
+// console.log(arr);
+// arr.push("New Element");
+// console.log(arr)
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift("First Element");
+// console.log(arr);
+
+
+//                     // <--------------------------------------------------------------------------------------------------------------->
+
+
+// const bhoot = ["manvendra", "bhallaldev", "ranasur", "bhimdev", "jeetdev"];
+// for(let i = 0; i<bhoot.length; i++) {
+//     console.log(bhoot[i] + "👹");
+// }
+
+// for (let asur of bhoot) {
+//     console.log(asur + "😈")
+// }
+
+//                     // <--------------------------------------------------------------------------------------------------------------->
+   
+// console.log("<------>")
+// const student1 = {
+//     name: "Lakshay",
+//     age: 18,
+//     course: "B.Tech"
+// }
+// for(let key in student1) {
+//     console.log(student1[key])
+// }
